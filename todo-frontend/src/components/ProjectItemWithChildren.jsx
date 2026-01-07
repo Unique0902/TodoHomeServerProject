895fa547da888cfc26a617e3e7ace1639c31c452
unique0902 <rla0591@naver.com>
2026-01-07 16:40:56 +0900

    프로젝트 페이지 토글 오른쪽으로 가게하기

--- todo-frontend/src/components/ProjectItemWithChildren.jsx
+++ todo-frontend/src/components/ProjectItemWithChildren.jsx
@@ -51,20 +51,7 @@ const ProjectItemWithChildren = ({
           <div className='indent-space' style={{ width: `${level * 20}px` }} />
         )}
 
-        {/* 토글 버튼 (하위 프로젝트가 있는 경우만) */}
-        {subProjects.length > 0 ? (
-          <button
-            className={`project-toggle-button ${isExpanded ? 'expanded' : ''}`}
-            onClick={handleToggleClick}
-            type='button'
-          >
-            {isExpanded ? '▼' : '▶'}
-          </button>
-        ) : (
-          <div className='project-toggle-spacer' />
-        )}
-
-        {/* 프로젝트 아이템 (토글 버튼 제외한 부분) */}
+        {/* 프로젝트 아이템 */}
         <div className='project-item-container' onClick={handleDetailClick}>
           <ProjectItem
             project={project}
@@ -73,6 +60,17 @@ const ProjectItemWithChildren = ({
             disableNavigation={true}
           />
         </div>
+
+        {/* 토글 버튼 (하위 프로젝트가 있는 경우만, 오른쪽 끝에 배치) */}
+        {subProjects.length > 0 && (
+          <button
+            className={`project-toggle-button ${isExpanded ? 'expanded' : ''}`}
+            onClick={handleToggleClick}
+            type='button'
+          >
+            {isExpanded ? '▼' : '▶'}
+          </button>
+        )}
       </div>
 
       {/* 하위 프로젝트 리스트 (토글이 펼쳐진 경우만) */}
